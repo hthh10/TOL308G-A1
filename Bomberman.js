6cc60da06efa08a0822bace9e30426cd69db1830
jandrih <jandrih@gmail.com>
2015-11-09 21:19:54 +0000

    getWallId og checkForWall komin í Entity.js

--- Bomberman.js
+++ Bomberman.js
@@ -161,29 +161,6 @@ Bomberman.prototype.computePosition = function () {
     }
 };
 
-Bomberman.prototype.getWallId = function (cx,cy) {
-	var xId = Math.floor((cx-g_playzone[0][0]) / g_sprites.wall.width);
-	var yId = Math.floor((cy-g_playzone[1][0]) / g_sprites.wall.height);
-	console.log(xId,yId);
-	return [yId, xId];
-};
-
-Bomberman.prototype.checkForWall = function (yId,xId) {
-    
-	if(wall.baseWall[yId][xId] > 0) return true;
-    else return false;
-
-};
-/*
-Bomberman.prototype.bounce = function (x,y) {
-    console.log("bouncing to.. ", x,y);
-
-    this.cx = LEVEL_OFFSET_X + (y*40);
-    this.cy = 110 + (x*40);
-
-    console.log("arriving at..", this.cx,this.cy);
-
-}*/
 // athugar collision við sprengju og breytir hraðanum eftir því
 Bomberman.prototype.isCollidingWithBomb = function (bomba) {
   if (this.cy > bomba.cy) this.cy += NOMINAL_WALKSPEED;
@@ -201,13 +178,15 @@ Bomberman.prototype.maybeDropBomb = function () {
         //    this.noBombs -= 1;
 
 
-            // Always drop bombs in center of the square
-            // Some magic numbers: cx: 40, cy: 110 is the center of the first 
-            var baseCx = 20, baseCy = 90; // Magic numbers, due to level being uneven.
-            var xPos = (Math.floor((this.cx-baseCx) / g_sprites.wall.width)), yPos = Math.floor((this.cy-baseCy) / g_sprites.wall.height);
-            var bombCx = g_sprites.wall.width + (g_sprites.wall.width * xPos), bombCy = 110 + (g_sprites.wall.height * yPos);
+        // Always drop bombs in center of the square
+        // Some magic numbers: cx: 40, cy: 110 is the center of the first 
+        var baseCx = g_playzone[0][0], baseCy = g_playzone[1][0];
+        var xPos = Math.floor((this.cx-baseCx) / g_sprites.wall.width), 
+			yPos = Math.floor((this.cy-baseCy) / g_sprites.wall.height);
+        var bombCx = g_sprites.wall.width + (g_sprites.wall.width * xPos), 
+			bombCy = 110 + (g_sprites.wall.height * yPos);
 
-            entityManager.dropBomb(bombCx, bombCy, xPos, yPos);
+        entityManager.dropBomb(bombCx, bombCy, xPos, yPos);
         //}
     }
 
