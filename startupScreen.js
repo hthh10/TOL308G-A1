b1e1d1b102775135c8fcfc63840d1f07c4afb8bf
berglindl <berglindpuma@gmail.com>
2015-11-20 10:28:48 +0000

    Smá startup screen lagfæringar

--- startupScreen.js
+++ startupScreen.js
@@ -7,8 +7,6 @@ var TOGGLE_OPTIONS = 'V'.charCodeAt(0);
 var startupScreen = {
 
 
-
-
 render : function(ctx) {
 	if(eatKey(TOGGLE_OPTIONS)) g_showInfo = !g_showInfo;
 	var oldFillStyle = ctx.fillStyle;
@@ -40,15 +38,12 @@ render : function(ctx) {
 		ctx.font="16px Arial";
 		ctx.fillText("Player 1 controls", g_canvas.width/2,140);
 		ctx.fillText("WASD - Move player 1 ", g_canvas.width/2,160);
-		ctx.fillText("E - PLayer 1 drop bomb ", g_canvas.width/2,180);
-		ctx.fillText("Q - PLayer 1 detonate bomb (if active) ", g_canvas.width/2,200);
-			ctx.fillText("Player 2 controls", g_canvas.width/2,240);
+		ctx.fillText("E - Player 1 drop bomb ", g_canvas.width/2,180);
+		ctx.fillText("Q - Player 1 detonate bomb (if active) ", g_canvas.width/2,200);
+		ctx.fillText("Player 2 controls", g_canvas.width/2,240);
 		ctx.fillText("IJKL - Move player 1 ", g_canvas.width/2,260);
-		ctx.fillText("O - PLayer 1 drop bomb ", g_canvas.width/2,280);
-		ctx.fillText("U - PLayer 1 detonate bomb (if active) ", g_canvas.width/2,300);
-
-
-
+		ctx.fillText("O - Player 2 drop bomb ", g_canvas.width/2,280);
+		ctx.fillText("U - Player 2 detonate bomb (if active) ", g_canvas.width/2,300);
 
     }
 	ctx.restore();
